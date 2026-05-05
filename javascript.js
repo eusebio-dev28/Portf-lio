b20bb9d1f5f7be0c2f5f78d325e1168adf2646cf
Eusebio Jimbi <156830441+eusebio-dev28@users.noreply.github.com>
2026-05-05 09:36:35 -0300

    Meu Portfólio

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var cursor= document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function(e){
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
